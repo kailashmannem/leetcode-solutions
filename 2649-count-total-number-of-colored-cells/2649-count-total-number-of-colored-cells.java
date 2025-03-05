93169b0a8b410f6376fc579a95f0d05712ff66f0
Kailash Chandra <155460988+kailashmannem@users.noreply.github.com>
2025-03-05 23:46:53 +0530

    Time: 0 ms (100%), Space: 40.4 MB (73.36%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -1,6 +1,5 @@
 class Solution {
     public long coloredCells(int n) {
-        long sum = (2*n-1)+2*((long)Math.pow(n-1,2));
-        return sum;
+        return (long) (2 * n - 1) + 2 * ((long) Math.pow(n - 1, 2));
     }
 }
