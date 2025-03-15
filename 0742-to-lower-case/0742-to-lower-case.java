18dd853c46aef585cfff91effc0db64db42c6ec5
Kailash Chandra <155460988+kailashmannem@users.noreply.github.com>
2025-03-15 13:13:35 +0530

    Time: 0 ms (100%), Space: 41.6 MB (77.6%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,5 +1,11 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        char[] str = s.toCharArray();
+        for (int i=0;i<str.length;i++) {
+            if (65 <= (int)str[i] && (int)str[i] <= 90) {
+                str[i] = (char) ((int) (str[i]) + 32);
+            }
+        }
+        return new String(str);
     }
 }
