147f7d93129d7fb98a0eacfcf230aa2fde319b78
Kailash Chandra <155460988+kailashmannem@users.noreply.github.com>
2025-03-23 18:24:00 +0530

    Time: 11 ms (28.34%), Space: 18 MB (28.88%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.py
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.py
@@ -2,7 +2,7 @@ class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
         d = {}
         for num in arr:
-            bc = self.bitsCount(num)
+            bc = bin(num).count('1')
             if bc not in d:
                 d[bc] = SortedList()
             d[bc].add(num)
@@ -10,10 +10,3 @@ class Solution:
         for key in sorted(d.keys()):
             res.extend(d[key])
         return res
-
-    def bitsCount(self, n: int) -> int:
-        count = 0
-        while n:
-            n &= (n - 1)
-            count += 1
-        return count
