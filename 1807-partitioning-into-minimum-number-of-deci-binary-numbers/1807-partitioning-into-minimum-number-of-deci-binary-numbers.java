df4b15bfc283175aabba873b3b55a2d4ab2440af
Kailash Chandra <155460988+kailashmannem@users.noreply.github.com>
2025-05-11 11:48:19 +0530

    Time: 5 ms (83.98%), Space: 45.2 MB (81.34%) - LeetHub

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -2,8 +2,8 @@ class Solution {
     public int minPartitions(String n) {
         //Maximum number of the string because we need n number of 1's to continuously add to make up to that number.
         int max = 0;
-        for (int i = 0; i<n.length(); i++) {
-            max = Math.max(max, n.charAt(i) - '0');
+        for (char c : n.toCharArray()) {
+            max = Math.max(max, c - '0');
         }
         return max;
     }
