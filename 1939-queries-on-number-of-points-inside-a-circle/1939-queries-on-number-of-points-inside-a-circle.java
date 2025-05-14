348b84ff2c365c4e06a7ad11aa8174aef1b73f35
Kailash Chandra <155460988+kailashmannem@users.noreply.github.com>
2025-05-14 19:30:08 +0530

    Time: 14 ms (93.11%), Space: 45.7 MB (11.29%) - LeetHub

--- 1939-queries-on-number-of-points-inside-a-circle/1939-queries-on-number-of-points-inside-a-circle.java
+++ 1939-queries-on-number-of-points-inside-a-circle/1939-queries-on-number-of-points-inside-a-circle.java
@@ -1,19 +1,16 @@
 class Solution {
     public int[] countPoints(int[][] points, int[][] queries) {
         int[] res = new int[queries.length];
-        int i = 0;
-        for (int[] q : queries) {
-            int count = 0;
+        for (int i = 0; i < queries.length; i++) {
+            int x = queries[i][0], y = queries[i][1], r = queries[i][2], count = 0;
             for (int[] p : points) {
-                int dx = p[0] - q[0];
-                int dy = p[1] - q[1];
-                int r = q[2];
+                int dx = p[0] - x;
+                int dy = p[1] - y;
                 if (dx*dx + dy*dy <= r*r) {
                     count++;
                 }
             }
             res[i] = count;
-            i++;
         }
         return res;
     }
