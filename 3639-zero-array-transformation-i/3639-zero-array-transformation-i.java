907a09425c5c2d81c474b20ba481d6427acf8e86
Kailash Chandra <155460988+kailashmannem@users.noreply.github.com>
2025-05-20 19:27:16 +0530

    Time: 3 ms (79.52%), Space: 95.8 MB (45.63%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
@@ -7,9 +7,7 @@ class Solution {
         }
         for (int i = 1; i < res.length; i++) {
             res[i] += res[i-1];
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > res[i]) return false;
+            if (nums[i-1] > res[i-1]) return false;
         }
         return true;
     }
