217846eac83edac4c118938a1bbbc5db6525a240
Kailash Chandra <155460988+kailashmannem@users.noreply.github.com>
2025-05-25 14:03:48 +0530

    Time: 42 ms (90.25%), Space: 35.6 MB (40.25%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
@@ -3,18 +3,12 @@ class Solution:
         wd = Counter(words)
         count = 0
         used = False
-        for s in words:
-            rev = s[::-1]
-            if s == rev:
-                pair = wd[s]//2
-                count += pair*4
-                wd[s] -= pair*2
-                if not used and wd[s] > 0:
-                    count += 2
+        for word in wd.keys():
+            rev = word[::-1]
+            if word == rev:
+                count += (wd[word]//2)*4
+                if wd[word]%2 == 1:
                     used = True
-            elif s < rev and rev in wd:
-                pair = min(wd[s],wd[rev])
-                count += pair*4
-                wd[s] -= pair
-                wd[rev] -= pair
-        return count
+            elif word < rev:
+                count += 4*min(wd[word],wd[rev])
+        return count + 2 if used else count
