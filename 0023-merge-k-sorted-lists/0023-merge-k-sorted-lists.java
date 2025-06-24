93c22ac14e43fa36164f0b7994be28fdf6a5d498
Kailash Chandra <155460988+kailashmannem@users.noreply.github.com>
2025-06-24 19:35:09 +0530

    Time: 5 ms (37.99%), Space: 44.3 MB (79.06%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,29 +10,44 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> queue = new PriorityQueue<>((a,b) -> a.val - b.val);
+        for (ListNode node : lists) {
+            if (node!=null) queue.add(node);
+        }
         ListNode result = new ListNode(Integer.MIN_VALUE);
-        for (int i = 0; i < lists.length; i++) {
-            ListNode temp = lists[i];
-            while (temp!=null) {
-                insert(temp.val,result);
-                temp = temp.next;
-            }
+        ListNode tail = result;
+        while (!queue.isEmpty()) {
+            ListNode curr  = queue.poll();
+            tail.next = curr;
+            tail = tail.next;
+            if (curr.next!=null) queue.add(curr.next);
         }
         return result.next;
     }
-    private void insert(int val, ListNode head) {
-        ListNode prev = null, curr = head;
-        ListNode node = new ListNode(val);
-        if (val <= head.val) {
-            node.next = head;
-            head = node;
-            return;
-        }
-        while (curr != null && val >= curr.val) {
-            prev = curr;
-            curr = curr.next;
-        }
-        node.next = prev.next;
-        prev.next = node;
-    }
+    // public ListNode mergeKLists(ListNode[] lists) {
+    //     ListNode result = new ListNode(Integer.MIN_VALUE);
+    //     for (int i = 0; i < lists.length; i++) {
+    //         ListNode temp = lists[i];
+    //         while (temp!=null) {
+    //             insert(temp.val,result);
+    //             temp = temp.next;
+    //         }
+    //     }
+    //     return result.next;
+    // }
+    // private void insert(int val, ListNode head) {
+    //     ListNode prev = null, curr = head;
+    //     ListNode node = new ListNode(val);
+    //     if (val <= head.val) {
+    //         node.next = head;
+    //         head = node;
+    //         return;
+    //     }
+    //     while (curr != null && val >= curr.val) {
+    //         prev = curr;
+    //         curr = curr.next;
+    //     }
+    //     node.next = prev.next;
+    //     prev.next = node;
+    // }
 }
