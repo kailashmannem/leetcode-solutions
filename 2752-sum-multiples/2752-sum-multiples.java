257544e472f0250ee84af46066e5c043ec661f67
Kailash Chandra <155460988+kailashmannem@users.noreply.github.com>
2025-08-11 21:37:22 +0530

    Time: 2 ms (95.66%), Space: 40.8 MB (82.98%) - LeetHub

---
+++ 2752-sum-multiples/2752-sum-multiples.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int sumOfMultiples(int n) {
+        int sum = 0;
+        for (int num = 3; num <= n; num++) {
+            if (num%3 == 0 || num%5 == 0 || num%7 == 0) sum += num;
+        }
+        return sum;
+    }
+}
