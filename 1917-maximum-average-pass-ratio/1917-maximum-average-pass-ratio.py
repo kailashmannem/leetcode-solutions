93d25d49148467e8fff98e47984d5899482cb694
Kailash Chandra <155460988+kailashmannem@users.noreply.github.com>
2025-09-01 21:30:17 +0530

    Time: 1078 ms (86.09%), Space: 54.8 MB (68.26%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.py
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.py
@@ -1,18 +1,20 @@
 class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
         heap = []
+        # Store in max_heap to get maximum difference in adding student
         for p, t in classes:
             gain = (p + 1)/(t + 1) - p/t
             heapq.heappush(heap, (-gain, p, t))
+        # Now to progressively add the difference and add back to heap
         for _ in range(extraStudents):
             e, p, t = heapq.heappop(heap)
             p += 1
             t += 1
             gain = (p + 1)/(t + 1) - p/t
             heapq.heappush(heap, (-gain, p, t))
+        # calculate the value
         total = 0
-        while heap:
-            _, p, t = heapq.heappop(heap)
+        for _, p, t in heap:
             total += p/t
         total /= len(classes)
         return total
