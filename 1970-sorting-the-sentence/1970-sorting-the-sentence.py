06fe917d8f76f27d77ff3d925cc5a8b06d81595a
Kailash Chandra <155460988+kailashmannem@users.noreply.github.com>
2025-09-09 22:50:54 +0530

    Time: 36 ms (70.11%), Space: 17.6 MB (65.22%) - LeetHub

--- 1970-sorting-the-sentence/1970-sorting-the-sentence.py
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.py
@@ -1,7 +1,5 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
         s = s.split()
-        res = [""]*len(s)
-        for st in s:
-            res[int(st[-1]) - 1] = st[:-1]
-        return " ".join(res)
+        s = sorted(s, key = lambda word : int(word[-1]))
+        return " ".join(word[:-1] for word in s)
