5283b562c4b14b5566fda71ac53ec318eb8d677f
Kailash Chandra <155460988+kailashmannem@users.noreply.github.com>
2025-09-19 21:06:03 +0530

    Time: 86 ms (68.78%), Space: 126.9 MB (86.85%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -1,8 +1,8 @@
 class Solution {
-    private Map<Integer, List<Integer>> build_graph(int n, int[][] edges) {
-        Map<Integer, List<Integer>> adj = new HashMap<>();
+    private List<List<Integer>> build_graph(int n, int[][] edges) {
+        List<List<Integer>> adj = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            adj.put(i, new ArrayList<Integer>());
+            adj.add(new ArrayList<Integer>());
         }
         for (int[] edge : edges) {
             adj.get(edge[0]).add(edge[1]);
@@ -10,7 +10,7 @@ class Solution {
         }
         return adj;
     }
-    private boolean findPath(Map<Integer, List<Integer>> adj, boolean[] visited, int source, int destination) {
+    private boolean findPath(List<List<Integer>> adj, boolean[] visited, int source, int destination) {
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(source);
         while (!queue.isEmpty()) {
@@ -26,7 +26,7 @@ class Solution {
         return false;
     }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        Map<Integer, List<Integer>> graph = build_graph(n, edges);
+        List<List<Integer>> graph = build_graph(n, edges);
         return findPath(graph, new boolean[n], source, destination);
     }
 }
