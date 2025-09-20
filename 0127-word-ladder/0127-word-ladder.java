92d1e1cacf3f9ddd24d51c32e956dcb5ba2d1c4e
Kailash Chandra <155460988+kailashmannem@users.noreply.github.com>
2025-09-20 14:49:52 +0530

    Time: 62 ms (86.31%), Space: 46.3 MB (66.81%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,7 +1,7 @@
 class Solution {
-    private class Pair<T,V> {
-        T word; V count;
-        Pair(T word, V count) {
+    private class Pair {
+        String word; int count;
+        Pair(String word, int count) {
             this.word = word;
             this.count = count;
         }
@@ -9,10 +9,10 @@ class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> words = new HashSet<>(wordList);
         if (!words.contains(endWord)) return 0;
-        Queue<Pair<String, Integer>> queue = new LinkedList<>();
-        queue.offer(new Pair<String, Integer>(beginWord, 1));
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(beginWord, 1));
         while (!queue.isEmpty()) {
-            Pair<String, Integer> pair = queue.poll();
+            Pair pair = queue.poll();
             char[] arr = pair.word.toCharArray();
             for (int i = 0; i < arr.length; i++) {
                 char original = arr[i];
@@ -21,7 +21,7 @@ class Solution {
                     String word = new String(arr);
                     if(words.contains(word)) {
                         if (word.equals(endWord)) return pair.count + 1;
-                        queue.offer(new Pair<String, Integer>(word, pair.count + 1));
+                        queue.offer(new Pair(word, pair.count + 1));
                         words.remove(word);
                     }
                 }
