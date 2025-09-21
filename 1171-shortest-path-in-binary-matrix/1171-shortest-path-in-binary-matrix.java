5c227bc08578aea443dde2b43acef14bdbf647f6
Kailash Chandra <155460988+kailashmannem@users.noreply.github.com>
2025-09-21 19:24:47 +0530

    Time: 28 ms (19.5%), Space: 46.5 MB (6.43%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
@@ -2,16 +2,17 @@ class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         if (grid[0][0] != 0) return -1; 
         int m = grid.length, n = grid[0].length;
+        if (m == 1 && n == 1 && grid[0][0] == 0) return 1;
         int[][] directions = new int[][]{{-1,0}, {1,0}, {0,1}, {0,-1}, {1,1}, {-1,-1}, {-1,1}, {1, -1}};
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
         pq.offer(new int[]{1, 0, 0});
         while (!pq.isEmpty()) {
             int dist = pq.peek()[0], x = pq.peek()[1], y = pq.peek()[2];
-            if (x == m - 1 && y == n - 1) return dist;
             pq.poll();
             for (int[] d : directions) {
                 int nx = x + d[0], ny = y + d[1];
                 if (nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] == 0) {
+                    if (nx == m - 1 && ny == n - 1) return dist + 1;
                     pq.offer(new int[]{dist + 1, nx, ny});
                     grid[nx][ny] = 1;
                 }
