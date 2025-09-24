c4baa86e8a9407edbe4c4f35883f605b99890d8a
Kailash Chandra <155460988+kailashmannem@users.noreply.github.com>
2025-09-24 22:16:31 +0530

    Time: 10 ms (80.03%), Space: 49.4 MB (11.21%) - LeetHub

---
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int min = -1;
+        List<List<int[]>> adj = new ArrayList<>();
+        for (int i = 0; i <= n; i++) adj.add(new ArrayList<int[]>());
+        for (int[] time : times) adj.get(time[0]).add(new int[]{time[1], time[2]});
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[k] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[0], b[0]));
+        pq.offer(new int[]{0, k});
+        while (!pq.isEmpty()) {
+            int time = pq.peek()[0], node = pq.peek()[1];
+            pq.poll();
+            for (int[] adjNode : adj.get(node)) {
+                int nnode = adjNode[0], ntime = adjNode[1];
+                if (time + ntime < dist[nnode]) {
+                    dist[nnode] = time + ntime;
+                    pq.offer(new int[]{dist[nnode], nnode});
+                }
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            min = Math.max(min, dist[i]);
+        }
+        return (min == Integer.MAX_VALUE) ? -1 : min; 
+    }
+}
