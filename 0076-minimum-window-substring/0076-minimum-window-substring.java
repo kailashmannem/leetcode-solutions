18fd1a4a6899c7cad7e39ab9cde68de9e3d2e9be
Kailash Chandra <155460988+kailashmannem@users.noreply.github.com>
2025-09-25 22:55:26 +0530

    Time: 42 ms (18.49%), Space: 44.9 MB (73.39%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,6 +1,6 @@
 class Solution {
     public String minWindow(String s, String t) {
-        String res = "";
+        int[] res = new int[]{-1, -1};
         int min = Integer.MAX_VALUE;
         int[] target = new int[128];
         for (char ch : t.toCharArray()) target[ch] += 1; 
@@ -11,13 +11,13 @@ class Solution {
             while (isValid(valid, target)) {
                 if (min > right - left + 1) {
                     min = right - left + 1;
-                    res = s.substring(left, right + 1);
+                    res[0] = left; res[1] = right;
                 }
                 valid[s.charAt(left)] -= 1;
                 left += 1;
             }
         }
-        return res;
+        return (res[0] == -1 || res[1] == -1) ? "" : s.substring(res[0], res[1] + 1);
     }
     private boolean isValid(int[] arr, int[] target) {
         for (int i = 0; i < 128; i++) {
